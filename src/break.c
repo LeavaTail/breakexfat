f00a15fb4dbf53d3888a53b89d8a332b21331cdc
LeavaTail <starbow.duster@gmail.com>
2022-05-31 21:53:34 +0900

    Add feature to break VolumeLength

--- src/break.c
+++ src/break.c
@@ -20,6 +20,7 @@ static int break_boot_jumpboot(struct super_block *sb, struct cache *cache, int 
 static int break_boot_fsname(struct super_block *sb, struct cache *cache, int type);
 static int break_boot_zero(struct super_block *sb, struct cache *cache, int type);
 static int break_boot_partoff(struct super_block *sb, struct cache *cache, int type);
+static int break_boot_vollen(struct super_block *sb, struct cache *cache, int type);
 
 //! Array for break pattern information
 static struct break_pattern_information break_boot_info[] =
@@ -28,6 +29,7 @@ static struct break_pattern_information break_boot_info[] =
 	{"Invalid FileSystemName", false, 0, break_boot_fsname},
 	{"Not zero in MustBeZero", false, 0, break_boot_zero},
 	{"Invalid PartitionOffset", false, 0, break_boot_partoff},
+	{"Too small VolumeLength", false, 0, break_boot_vollen},
 };
 
 /**
@@ -144,6 +146,26 @@ static int break_boot_partoff(struct super_block *sb, struct cache *cache, int t
 
 	boot = cache->data;
 	boot->partition_offset = ULONG_MAX;
+	cache->dirty = true;
+
+	return 0;
+}
+
+/**
+ * @brief break VolumeLength in boot sector
+ * @param [in] sb    Filesystem metadata
+ * @param [in] cache boot sector cache
+ * @param [in] type  break pattern
+ *
+ * @retval 0 success
+ * @retval Negative failed
+ */
+static int break_boot_vollen(struct super_block *sb, struct cache *cache, int type)
+{
+	struct boot_sector *boot;
+
+	boot = cache->data;
+	boot->vol_length = (power2(20) / sb->sector_size) - 1;
 	cache->dirty = true;
 
 	return 0;
